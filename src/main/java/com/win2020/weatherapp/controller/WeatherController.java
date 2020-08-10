7e8c6d8893ec0a7696b8f1d5867adaa217d9e038
bransonperkins <bransonperkins@gmail.com>
2020-08-10 09:19:12 -0400

    update to RestController

--- src/main/java/com/win2020/weatherapp/controller/WeatherController.java
+++ src/main/java/com/win2020/weatherapp/controller/WeatherController.java
@@ -5,20 +5,21 @@ import com.win2020.weatherapp.model.Response;
 import com.win2020.weatherapp.model.ZipCode;
 import com.win2020.weatherapp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class WeatherController {
 
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping
+    @GetMapping("/")
     public String getIndex(Model model) {
         List<ZipCode> zipCodeList = weatherService.getRecentSearches();
         model.addAttribute("request", new Request());
@@ -26,8 +27,8 @@ public class WeatherController {
         return "index";
     }
 
-    @PostMapping
-    public String postIndex(Request request, Model model) {
+    @PostMapping("/current")
+    public String postIndex(@RequestBody Request request, Model model) {
         List<ZipCode> zipCodeList = weatherService.getRecentSearches();
         Response data = weatherService.getForecast(request.getZipCode());
         model.addAttribute("data", data);
